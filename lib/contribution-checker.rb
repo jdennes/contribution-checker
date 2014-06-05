108cc0f2d28b3a09af2cdf1b24fdcd865e54e983
James Dennes <jdennes@gmail.com>
2014-06-06 00:33:23 +0200

    Refactor style

--- lib/contribution-checker.rb
+++ lib/contribution-checker.rb
@@ -3,7 +3,7 @@ require "octokit"
 
 module ContributionChecker
 
-  def self.in_default_or_gh_pages(commit, repo)
+  def self.commit_in_valid_branch?(commit, repo)
     # If two refs are entirely different commit histories, the API responds with
     # a 404. Rescue Octokit::NotFound in this case.
     begin
@@ -16,7 +16,7 @@ module ContributionChecker
     # The compare status should be "identical" or "behind" if the commit is in
     # the default branch
     unless default_compare and
-      %w[identical behind].include? default_compare[:status]
+      %w(identical behind).include? default_compare[:status]
 
       # If the commit is not in the default branch, check the gh-pages branch
       begin
@@ -25,13 +25,13 @@ module ContributionChecker
         gh_pages_compare = nil
       end
       return false if !gh_pages_compare
-      return false if !%w[identical behind].include? gh_pages_compare [:status]
+      return false if !%w(identical behind).include? gh_pages_compare [:status]
     end
 
     true
   end
 
-  def self.authored_in_last_year(commit)
+  def self.commit_authored_in_last_year?(commit)
     a_year_ago = Time.now - (365.25 * 86400)
     commit_time = commit[:commit][:author][:date]
     (commit_time <=> a_year_ago) == 1
@@ -41,11 +41,11 @@ module ContributionChecker
     parts = URI.parse(url).path.split("/")
     nwo = "#{parts[1]}/#{parts[2]}"
     sha = parts[4]
-    @repo    = Octokit.repository nwo
-    @commit  = Octokit.commit nwo, sha
+    repo    = Octokit.repository nwo
+    commit  = Octokit.commit nwo, sha
 
-    return false unless in_default_or_gh_pages @commit, @repo
-    return false unless authored_in_last_year @commit
+    return false unless commit_in_valid_branch? commit, repo
+    return false unless commit_authored_in_last_year? commit
 
     true
   end

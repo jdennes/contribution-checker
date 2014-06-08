f982a61d9c0ca21f9eed6d121999542b8547bb58
James Dennes <jdennes@gmail.com>
2014-06-09 00:23:58 +0200

    Use webmock

--- spec/contribution-checker/checker_spec.rb
+++ spec/contribution-checker/checker_spec.rb
@@ -15,6 +15,22 @@ describe ContributionChecker::Checker do
       end
     end
 
+    context "when a valid URL is provided which isn't a commit URL" do
+      let(:checker) { checker = ContributionChecker::Checker.new \
+        :access_token => "your token",
+        :commit_url   => "https://example.com/"
+      }
+
+      before do
+        stub_request(:get, "https://api.github.com/repos/commits/").to_return(
+         :status  => 404, :body    => "")
+      end
+
+      it "raises an error" do
+        expect { checker.check }.to raise_error ContributionChecker::InvalidCommitUrlError
+      end
+    end
+
   end
 
 end
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,2 +1,5 @@
 require "rspec"
+require 'webmock/rspec'
 require "contribution-checker"
+
+WebMock.disable_net_connect!
